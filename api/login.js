e565776fe41e2b3f7b67c32b8125f5c626a4a281
AndRaxx <skystarywalker@gmail.com>
2026-04-15 10:57:48 +0800

    add jwt login

--- api/login.js
+++ api/login.js
@@ -1,12 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+const SECRET = process.env.JWT_SECRET;
+
 module.exports = (req, res) => {
   if (req.method !== "POST") {
     return res.status(405).end();
   }
 
-  const { username, password } = req.body;
+  const { username, password } = req.body || {};
+
+  if (!username || !password) {
+    return res.status(400).json({ error: "Data kosong" });
+  }
 
   if (username === "andraxx" && password === "BR2201") {
-    return res.json({ success: true });
+
+    const token = jwt.sign(
+      { username },
+      SECRET,
+      { expiresIn: "1d" }
+    );
+
+    return res.json({
+      success: true,
+      token
+    });
   }
 
   return res.json({ success: false });
